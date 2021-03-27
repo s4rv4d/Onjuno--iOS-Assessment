4148d470c1e7988f853103584c59f1961b2346c0
s4rv4d <sarvadshetty@gmail.com>
2021-03-27 10:27:17 +0300

    Initial Commit

---
+++ OnjunoTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OnjunoTest
+//
+//  Created by Sarvad Shetty on 27/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OnjunoTest/OnjunoTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnjunoTestApp.swift
+//  OnjunoTest
+//
+//  Created by Sarvad Shetty on 27/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct OnjunoTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
